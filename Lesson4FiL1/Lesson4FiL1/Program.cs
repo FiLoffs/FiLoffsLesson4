4e6a7b358e77fcdb48b68432799f383ad1a02350
FiLoffs <filod72@gmail.com>
2019-03-04 18:42:33 +0200

    тренируемся на занятии

---
+++ Lesson4FiL1/Lesson4FiL1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4FiL1
+{
+    class Program
+    {
+        //упражнение на занятии
+        static void Main(string[] args)
+        {
+            for (int a = 1; a <= 10; a++)
+
+            {
+
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.Write(Math.Pow(a,j)+"\t"); 
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
